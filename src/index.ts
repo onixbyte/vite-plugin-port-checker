e5224e52d2e6dee1355be340bcf94cdaff80f02d
Zihlu Wang <real@zihluwang.me>
2026-05-08 16:09:37 +0800

    feat: add missing restricted ports

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const RESTRICTED_PORTS = new Set([
   1, 7, 9, 11, 13, 15, 17, 19, 20, 21, 22, 23, 25, 37, 42, 43, 53, 77, 79, 87, 95, 101, 102, 103,
   104, 109, 110, 111, 113, 115, 117, 119, 123, 135, 139, 143, 179, 389, 465, 512, 513, 514, 515,
   526, 530, 531, 532, 540, 556, 563, 587, 601, 636, 993, 995, 2049, 3659, 4045, 6000, 6665, 6666,
-  6667, 6668, 6669,
+  6667, 6668, 6669, 6679, 6697, 10080
 ])
 
 function checkRestrictedPortPlugin(): Plugin {
@@ -22,7 +22,6 @@ function checkRestrictedPortPlugin(): Plugin {
           const port = address.port
 
           if (RESTRICTED_PORTS.has(port)) {
-            // Using British English for the console warning messages
             console.warn(
               `\x1b[33m%s\x1b[0m`, // Yellow colour output
               `\n[Warning] The current listening port ${port} is categorised as a restricted port by most browsers.`
